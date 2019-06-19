2a7cc3e1e4a3bc3b8d053db8a9fd4639b5f215a5
wbs <512742341@qq.com>
2019-06-19 15:35:36 +0800

    add log ext

---
+++ SSS.Infrastructure.Util/Log/ApplicationLog.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SSS.Infrastructure.Util.Log
+{
+    public static class ApplicationLog
+    {
+        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+        public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+    }
+}
