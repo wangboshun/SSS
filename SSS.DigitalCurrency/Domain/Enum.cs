b429593c9a2e8d340a418d73eb656ef96d2090fd
wbs <512742341@qq.com>
2019-12-26 17:23:18 +0800

    update  code

--- SSS.DigitalCurrency/Domain/Enum.cs
+++ SSS.DigitalCurrency/Domain/Enum.cs
@@ -1,26 +1,5 @@
-﻿using System;
-
-namespace SSS.DigitalCurrency.Domain
+﻿namespace SSS.DigitalCurrency.Domain
 {
-    public class KLine
-    {
-        public int id { set; get; }
-        public double amount { set; get; }
-        public int count { set; get; }
-        public double open { set; get; }
-        public double close { set; get; }
-        public double low { set; get; }
-        public double high { set; get; }
-        public double vol { set; get; }
-        public DateTime time { set; get; }
-    }
-
-    public class AvgPrice
-    {
-        public DateTime time { set; get; }
-        public double price { set; get; }
-    }
-
     public class CoinSymbols
     {
         public string base_currency { set; get; }
