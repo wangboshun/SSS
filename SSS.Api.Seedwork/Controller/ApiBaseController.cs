8ca1040561b3f769b65487a259d107204b767759
wbs <512742341@qq.com>
2020-01-05 22:39:42 +0800

    update httpcode

--- SSS.Api.Seedwork/Controller/ApiBaseController.cs
+++ SSS.Api.Seedwork/Controller/ApiBaseController.cs
@@ -13,18 +13,28 @@ using System.Collections.Generic;
 using System.Linq;
 
 /*
-    200 OK - [GET]：服务器成功返回用户请求的数据，该操作是幂等的（Idempotent）。
-    201 CREATED - [POST/PUT/PATCH]：用户新建或修改数据成功。
-    202 Accepted - [*]：表示一个请求已经进入后台排队（异步任务）
-    204 NO CONTENT - [DELETE]：用户删除数据成功。
-    400 INVALID REQUEST - [POST/PUT/PATCH]：用户发出的请求有错误，服务器没有进行新建或修改数据的操作，该操作是幂等的。
-    401 Unauthorized - [*]：表示用户没有权限（令牌、用户名、密码错误）。
-    403 Forbidden - [*] 表示用户得到授权（与401错误相对），但是访问是被禁止的。
-    404 NOT FOUND - [*]：用户发出的请求针对的是不存在的记录，服务器没有进行操作，该操作是幂等的。
-    406 Not Acceptable - [GET]：用户请求的格式不可得（比如用户请求JSON格式，但是只有XML格式）。
-    410 Gone -[GET]：用户请求的资源被永久删除，且不会再得到的。
-    422 Unprocesable entity - [POST/PUT/PATCH] 当创建一个对象时，发生一个验证错误。
-    500 INTERNAL SERVER ERROR - [*]：服务器发生错误，用户将无法判断发出的请求是否成功。
+1xx，属于信息性的状态码。Web API并不使用1xx的状态码。 
+
+2xx，意味着请求执行的很成功。 
+    200 - Ok，表示请求成功； 
+    201 - Created，请求成功并创建了资源； 
+    204 - No Content，请求成功，但是不应该返回任何东西，例如删除操作。 
+
+3xx，用于跳转。例如告诉搜素引擎，某个页面的网址已经永久的改变了。绝大多数的Web API都不需要使用这类状态码。 
+
+4xx，客户端错误： 
+    400 - Bad Request，表示API消费者发送到服务器的请求是有错误的； 
+    401 - Unauthorized，表示没有提供授权信息或者提供的授权信息不正确； 
+    403 - Forbidden，表示身份认证已经成功，但是已认证的用户却无法访问请求的资源； 
+    404 - Not Found，表示请求的资源不存在； 
+    405 - Method not allowed，当尝试发送请求到资源的时候，使用了不被支持的HTTP方法时，就会返回405状态码； 
+    406 - Not acceptable，这表示API消费者请求的表述格式并不被Web API所支持，并且API不会提供默认的表述格式。例如请求的媒体类型是application/xml，但是Web API仅支持application/json类型，并且API不会将application/json作为默认格式提供； 
+    409 - Conflict，表示请求与服务器当前状态冲突。通常指更新资源时发生的冲突，例如，当你编辑某个资源的时候，该资源在服务器上又进行了更新，所以你编辑的资源版本和服务器的不一致。当然有时候也用来表示你想要创建的资源在服务器上已经存在了。它就是用来处理并发问题的状态码。  
+    415 - Unsupported media type，与406正好相反，有一些请求必须带着数据发往服务器，这些数据都属于特定的媒体类型，如果API不支持该媒体类型格式，415就会被返回。 
+    422 - Unprocessable entity，它是HTTP扩展协议的一部分。它说明服务器已经懂得了实体的Content Type，也就是说415状态码肯定不合适；此外，实体的语法也没有问题，所以400也不合适。但是服务器仍然无法处理这个实体数据，这时就可以返回422。所以它通常是用来表示语意上有错误，通常就表示实体验证的错误。 
+
+5xx，服务器错误： 
+    500 - Internal server error，表示服务器出现了错误，客户端无能为力，只能以后再试试了。 
  */
 
 namespace SSS.Api.Seedwork.Controller
@@ -67,7 +77,7 @@ namespace SSS.Api.Seedwork.Controller
         {
             if (data != null)
                 return ApiResponse(data, true, "新增成功", 200);
-            return ApiResponse(data, false, "新增失败", 402);
+            return ApiResponse(data, false, "新增失败", 204);
         }
 
         /// <summary>
@@ -80,7 +90,7 @@ namespace SSS.Api.Seedwork.Controller
         {
             if (data != null & status)
                 return ApiResponse(data, true, "删除成功", 200);
-            return ApiResponse(data, false, "删除失败", 402);
+            return ApiResponse(data, false, "删除失败", 204);
         }
 
         /// <summary>
@@ -93,7 +103,7 @@ namespace SSS.Api.Seedwork.Controller
         {
             if (data != null & status)
                 return ApiResponse(data, true, "修改成功", 200);
-            return ApiResponse(data, false, "修改失败", 402);
+            return ApiResponse(data, false, "修改失败", 204);
         }
 
         /// <summary>
@@ -111,7 +121,7 @@ namespace SSS.Api.Seedwork.Controller
                 else
                     return ApiResponse(null, true, "分页数据为空", 200);
             }
-            return ApiResponse(null, false, "获取分页失败", 402);
+            return ApiResponse(null, false, "获取分页失败", 204);
         }
 
         /// <summary>
@@ -124,7 +134,7 @@ namespace SSS.Api.Seedwork.Controller
         {
             if (data != null)
                 return ApiResponse(data, true, "操作成功", 200);
-            return ApiResponse(null, false, "操作失败", 402);
+            return ApiResponse(null, false, "操作失败", 204);
         }
 
         /// <summary>
@@ -136,7 +146,7 @@ namespace SSS.Api.Seedwork.Controller
         {
             if (status)
                 return ApiResponse(true, true, "操作成功", 200);
-            return ApiResponse(true, false, "操作失败", 402);
+            return ApiResponse(true, false, "操作失败", 204);
         }
 
         protected IActionResult ApiResponse(object data, bool status, string message, int code)
@@ -144,7 +154,7 @@ namespace SSS.Api.Seedwork.Controller
             if (!status)
             {
                 if (!IsValidOperation())
-                    return BadRequest(new { status = false, data = "", message = Notice.Select(n => n.ErrorMessage), code = 402 });
+                    return BadRequest(new { status = false, data = "", message = Notice.Select(n => n.ErrorMessage), code = code });
 
                 return Accepted(new { status, data, message, code });
             }
@@ -156,7 +166,7 @@ namespace SSS.Api.Seedwork.Controller
                 return Ok(new { status, data, message, code });
             }
 
-            return BadRequest(new { status = false, data = "", message = Notice.Select(n => n.ErrorMessage), code = 402 });
+            return BadRequest(new { status = false, data = "", message = Notice.Select(n => n.ErrorMessage), code = code });
         }
     }
 }
