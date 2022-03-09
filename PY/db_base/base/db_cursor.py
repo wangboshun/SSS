f0863d51bf242642933ffb4595c3b71ee267fef0
wbs <512742341@qq.com>
2022-03-09 17:14:10 +0800

    change director

--- PY/db_base/base/db_cursor.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class db_cursorEnum(Enum):
-    Cursor = 1
-    DictCursor = 2
-    SSCursor = 3
-    SSDictCursor = 4
--- PY/db_base/base/db_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class db_typeEnum(Enum):
-    MySQL = 1
-    MSSQL = 2
-    PGSQL = 3
-    CK = 4
-    SQLITE = 5
