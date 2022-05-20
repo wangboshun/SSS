21c19f63b1856eadef950641416c34e5ab2972bc
wbs <512742341@qq.com>
2022-05-20 11:04:57 +0800

    更新sql

--- Java/data_sync/src/main/java/com/mapper/GenSQL.java
+++ Java/data_sync/src/main/java/com/mapper/GenSQL.java
@@ -4,17 +4,12 @@ import org.apache.ibatis.jdbc.SQL;
 
 public class GenSQL {
     public String getSQL(final String id) {
-        return new SQL() {
-            //这个大括号 -> {} 表示构造代码块，表示每一次创建此对象，都会执行构造代码块中的代码
-            {
-                //表示查询学生表中的左右记录
-                SELECT("*");
-                FROM("Test1");
-                if (id != null) {
-                    WHERE("ID=#{id}");
-                }
-            }
-            //toString()方法表示将构造的sql语句转化为一个字符串，并返回
-        }.toString();
+        SQL s = new SQL();
+        s.SELECT("*");
+        s.FROM("Test1");
+        if (id != null) {
+            s.WHERE("ID=#{id}");
+        }
+        return s.toString();
     }
 }
