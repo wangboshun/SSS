0aea0e48615c4dfb590207604c394f66d8ea80a8
wbs <512742341@qq.com>
2022-06-09 17:13:00 +0800

    添加redis

---
+++ Java/quality/src/main/java/org/wbs/quality/infra/ConfigurationUtils.java
@@ -0,0 +1,47 @@
+package org.wbs.quality.infra;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.wbs.quality.infra.utils.redis.RedisUtils;
+import org.wbs.quality.infra.utils.yaml.YamlUtils;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Properties;
+
+/**
+ * @author WBS
+ * Date:2022/6/9
+ */
+
+@Configuration
+public class ConfigurationUtils {
+
+    @Bean("yamlConfig")
+    public YamlUtils yamlConfig() {
+        Resource app = new ClassPathResource("application.yml");
+        YamlPropertiesFactoryBean yamlPropertiesFactoryBean = new YamlPropertiesFactoryBean();
+        yamlPropertiesFactoryBean.setResources(app);
+        Properties properties = yamlPropertiesFactoryBean.getObject();
+        return new YamlUtils(properties);
+    }
+
+    @Bean
+    @DependsOn("yamlConfig")  //需要依赖yamlConfig
+    public RedisUtils redisConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        String host = YamlUtils.getString("spring.redis.host");
+        String password = YamlUtils.getString("spring.redis.password");
+        Integer port = YamlUtils.getInteger("spring.redis.port");
+        Integer database = YamlUtils.getInteger("spring.redis.database");
+        //连接超时（默认2000ms）
+        JedisPool pool = new JedisPool(jedisPoolConfig, host, port, 2000, password, database);
+        return new RedisUtils(pool);
+    }
+
+
+}
---
+++ Java/quality/src/main/java/org/wbs/quality/infra/utils/RedisUtils.java
@@ -0,0 +1,26 @@
+package org.wbs.quality.infra.utils.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * @author WBS
+ * Date:2022/6/9
+ */
+
+public class RedisUtils {
+
+    private static Jedis redis;
+
+    public RedisUtils(JedisPool pool) {
+        RedisUtils.redis = pool.getResource();
+    }
+
+    public static void setString(String key, String value) {
+        redis.set(key, value);
+    }
+
+    public static String getString(String key) {
+        return redis.get(key);
+    }
+}
