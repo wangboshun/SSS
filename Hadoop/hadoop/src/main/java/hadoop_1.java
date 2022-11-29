91ede64c90a43800167bcefd2a83c7c9d285cfe5
wbs <512742341@qq.com>
2022-11-29 14:46:26 +0800

    删除iot模块

---
+++ Hadoop/hadoop/src/main/java/hadoop_1.java
@@ -0,0 +1,101 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+
+/**
+ * @author WBS
+ * Date 2022-11-26 11:02
+ * Main
+ */
+
+public class hadoop_1 {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+        try {
+            info();
+//            put();
+//            get();
+        } catch (Exception e) {
+
+        }
+
+        System.out.println("hello hadoop!");
+    }
+
+    private static void put() {
+        try {
+            URL url = new URL("hdfs://192.168.245.101:9000");
+            Configuration configuration = new Configuration();
+            FileSystem fs = FileSystem.get(url.toURI(), configuration, "wbs");
+            fs.copyFromLocalFile(true, true, new Path("C:\\Users\\WBS\\Desktop\\chabushuj.csv"), new Path("/test1"));
+            fs.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void get() throws URISyntaxException, IOException, InterruptedException {
+        try {
+            URL url = new URL("hdfs://192.168.245.101:9000");
+            Configuration configuration = new Configuration();
+            FileSystem fs = FileSystem.get(url.toURI(), configuration, "wbs");
+            fs.copyToLocalFile(false, new Path("/test1/chabushuj.csv"), new Path("C:\\Users\\WBS\\Desktop\\test\\aaa.csv"));
+            fs.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void mkdir() throws IOException, URISyntaxException, InterruptedException {
+        URL url = new URL("hdfs://192.168.245.101:9000");
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(url.toURI(), configuration, "wbs");
+        fs.mkdirs(new Path("/test2"));
+
+        fs.close();
+    }
+
+    private static void delete() throws IOException, URISyntaxException, InterruptedException {
+        URL url = new URL("hdfs://192.168.245.101:9000");
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(url.toURI(), configuration, "wbs");
+        fs.delete(new Path("/test1"), true);
+
+        fs.close();
+    }
+
+    private static void move() throws IOException, URISyntaxException, InterruptedException {
+        URL url = new URL("hdfs://192.168.245.101:9000");
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(url.toURI(), configuration, "wbs");
+        fs.rename(new Path("/test2"), new Path("/test1"));
+
+        fs.close();
+    }
+
+    private static void info() throws IOException, URISyntaxException, InterruptedException {
+        URL url = new URL("hdfs://192.168.245.101:9000");
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(url.toURI(), configuration, "wbs");
+        RemoteIterator<LocatedFileStatus> files = fs.listFiles(new Path("/"), true);
+        while (files.hasNext()) {
+            LocatedFileStatus status = files.next();
+            System.out.println("文件名：" + status.getPath().getName());
+            System.out.println("地址：" + status.getPath());
+            System.out.println("长度：" + status.getLen());
+            System.out.println("权限：" + status.getPermission());
+            System.out.println("类型：" + (status.isFile() ? "文件" : "文件夹"));
+
+            //查看块信息
+            BlockLocation[] blockLocations = status.getBlockLocations();
+            System.out.println(Arrays.toString(blockLocations));
+        }
+        fs.close();
+    }
+
+
+}
--- Java/sss/pipe/src/main/java/com/zny/pipe/controller/TableController.java
+++ Java/sss/pipe/src/main/java/com/zny/pipe/controller/TableController.java
@@ -24,11 +24,22 @@ public class TableController {
         this.tableConfigApplication = tableConfigApplication;
     }
 
+    /**
+     * 获取表信息配置
+     *
+     * @param id 源节点id
+     */
     @GetMapping(value = "/{id}")
     public SaResult get(String id) {
         return tableConfigApplication.getTableConfigById(id);
     }
 
+    /**
+     * 根据连接和表名获取表信息
+     *
+     * @param connectId 连接id
+     * @param tableName 表名
+     */
     @GetMapping(value = "/get_by_connect")
     public SaResult get(String connectId, String tableName) {
         List<TableConfigModel> list = tableConfigApplication.getByConnectId(connectId, tableName);
@@ -39,19 +50,36 @@ public class TableController {
         }
     }
 
+    /**
+     * 添加表配置
+     *
+     * @param connectId 连接id
+     * @param tableName 表名
+     */
     @PostMapping(value = "/add")
     public SaResult add(String connectId, String tableName) {
         return tableConfigApplication.addTableConfig(connectId, tableName);
     }
 
+    /**
+     * 更新表配置
+     *
+     * @param connectId 连接id
+     * @param tableName 表名
+     */
     @PostMapping(value = "/update")
     public SaResult update(String connectId, String tableName) {
         return tableConfigApplication.updateTableConfig(connectId, tableName);
     }
 
+    /**
+     * 删除表配置
+     *
+     * @param connectId 连接id
+     * @param tableName 表名
+     */
     @DeleteMapping(value = "/delete")
     public SaResult delete(String connectId, String tableName) {
         return tableConfigApplication.deleteTableConfig(connectId, tableName);
     }
-
 }
