06570a89fbe51430c46da32f3b34c7ac4eb1e6cd
wbs <512742341@qq.com>
2024-11-30 17:48:56 +0800

    添加java openiot

---
+++ Java/OpenIoT/OpenIoT/src/main/java/com/sss/openiot/OpenIoTApplication.java
@@ -0,0 +1,13 @@
+package com.sss.openiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIoTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenIoTApplication.class, args);
+    }
+
+}
---
+++ Java/OpenIoT/OpenIoT/src/test/java/com/sss/openiot/OpenIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.openiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenIoTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
